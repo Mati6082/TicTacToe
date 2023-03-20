cedcbb181fe1189e65d32e86cac23e4aeb38642a
matpakla <matpakla@gmail.com>
2023-03-20 12:31:58 +0100

    kolko krzyzyk

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,61 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
+import kotlin.reflect.typeOf
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    var board = Array(9) { 0 }
+    drawBoard(board)
+    getUserMove(board)
+    getBotMove(board)
+}
+
+fun checkBoard(board: Array<Int>) {
+    checkRows(board)
+}
+
+fun checkRows(board: Array<Int>):Int? {
+    var sum = 0
+    for (i in 0 .. 8) {
+        if (i % 3 == 0 && i != 0)
+            sum = 0
+        else sum += board[i]
+        if (sum == 3 || sum == 6) return sum
+    }
+    return null
+}
+
+fun drawBoard(board: Array<Int>) {
+    for (i in 0 until 9) {
+        if (i % 3 == 0 && i != 0) print("\n")
+        print("${board[i]} ")
+    }
+    print("\n\n")
+}
+
+fun getUserMove(board: Array<Int>) {
+    while (true) {
+        print("Podaj numer pola: ")
+        val userMove = readln().toInt()
+        if (userMove !in 1..9) {
+            println("\nNieprawidlowa wartosc\n")
+            continue
+        } else if (board[userMove-1] != 0) {
+            println("\nTo pole jest juz zajete\n")
+            continue
+        } else {
+            board[userMove-1] = 1
+            break
+        }
+    }
+    println()
+    drawBoard(board)
+}
+
+fun getBotMove(board: Array<Int>) {
+    println("Ruch komputera: \n")
+    while (true) {
+        val computerMove = Random.nextInt(0, 9)
+        if (board[computerMove] != 0) continue
+        else board[computerMove] = 2; break
+    }
+    drawBoard(board)
 }
